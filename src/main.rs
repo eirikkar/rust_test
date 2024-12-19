d43921e8618bcf8b1a3b54624339587fa867f8f2
eirikkar <eirikkar@proton.me>
2024-12-19 21:03:53 +0100

    Add guessing game to program

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 //Main function
 fn main() {
     println!("Guess the number");
 
     println!("Please input your guess: ");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
 
     io::stdin()
@@ -12,7 +14,11 @@ fn main() {
         .expect("failed to read line");
     let number: i32 = guess.trim().parse().expect("Input is not a integer!");
 
-    println!("You guessed: {}", number);
+    match number.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
     let x = 5;
     println!("The value of x is: {x}");
     another_function(5);
