fff7c5d9796cd4dc371fa57d2116b1e5016d22c7
eirikkar <eirikkar@proton.me>
2024-12-19 21:23:14 +0100

    Added more parsing functionality

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,20 @@ use std::{cmp::Ordering, io};
 fn main() {
     println!("Guess the number");
 
+    let secret_number = rand::thread_rng().gen_range(1..=10);
     loop {
         let mut guess = String::new();
         println!("Please input your guess: ");
         io::stdin()
             .read_line(&mut guess)
             .expect("failed to read line");
-        let number: i32 = guess.trim().parse().expect("Input is not a integer!");
+        let number: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a number");
+                continue;
+            }
+        };
 
         match number.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
@@ -22,4 +29,3 @@ fn main() {
         }
     }
 }
-//Another function
