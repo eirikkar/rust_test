4f817fc8071ee88700c19974d6d03aeb865080e4
eirikkar <eirikkar@proton.me>
2024-12-19 14:47:30 +0100

    Some testing

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    println!("Please input your guess: ");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You guessed: {}", guess);
+    let x = 5;
+    println!("The value of x is: {x}");
+    another_function();
+}
+fn another_function() {
+    println!("Another function");
+}
